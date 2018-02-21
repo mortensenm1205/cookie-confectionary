25dececc150736fb535ac319d0521491e11d5af1
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-02-21 12:13:57 -0700

    added a results element to display some sort of change of submit was clicked

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,6 +2,7 @@ $(function() {
 
   var cookieQuantitys = $("input");
   var btn = $("#btn");
+  var results = $("#results");
 
   function setCookies() {
     $.each(cookieQuantitys, (index, cookie) => {
@@ -9,9 +10,19 @@ $(function() {
     });
   };
 
+
   btn.on("click",  (e) => {
     e.preventDefault();
-    setCookies();
+
+    if(document.cookie) {
+      results.text("Damn you want to change the amount already? Clear your cookies first!")
+    } else {
+      setCookies();
+      cookieQuantitys.each((i) => {
+        results.append(` You choose ${Cookies.get(`${cookieQuantitys[i].name}`)} amount of cookies for ${cookieQuantitys[i].name}!  <br/>`);
+      });
+      results.append('Come back for more anytime!');
+    }
   });
 
 });
