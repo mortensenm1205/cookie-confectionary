bfb284573e169dca6628a02b10741fdc0a43ce08
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-02-21 12:19:11 -0700

    added clear html file for when user refreshes the page

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
   let cookies = req.cookies;
 
   if(Object.keys(cookies).length !== 0) {
-    res.send('Hey fatass clear your cookies');
+    res.sendFile(__dirname + '/views/clear.html');
   } else {
     res.sendFile(__dirname + '/views/index.html');
   }
